57e4a621066756665b470a6268ec14bf153ba8b5
jluoxin <13834144912@163.com>
2019-06-12 21:06:14 +0800

    部署

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -20,9 +20,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'USER': 'blueprod',                            # 你的数据库user
+        'PASSWORD': 'blueprod@2018',                        # 你的数据库password
+        'HOST': '10.130.44.108',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
