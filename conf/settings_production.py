3adfb221f986c572a0c192c1a7fb846a0f49ce4c
jluoxin <13834144912@163.com>
2019-06-13 10:29:16 +0800

    db update

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -19,7 +19,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'blueprod',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'blueprod',                            # 你的数据库user
         'PASSWORD': 'blueprod@2018',                        # 你的数据库password
         'HOST': '10.130.44.108',                   		   # 数据库HOST
